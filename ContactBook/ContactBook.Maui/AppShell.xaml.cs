f7cdceae7ca9e78427ea590f4a996ed1428d8d69
SHZ <shzworking@gmail.com>
2024-01-06 18:14:55 +0500

    Create Project

---
+++ ContactBook/ContactBook.Maui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactBook.Maui;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ContactBook/ContactBook.Maui/Views/ContactsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ContactBook.Maui.Views;
+
+public partial class ContactsPage : ContentPage
+{
+	public ContactsPage()
+	{
+		InitializeComponent();
+	}
+}
